22cc530954838a08fc9383b309fa5a1f789a6ae1
akash1297 <akash.zerox505@gmail.com>
2020-12-10 19:07:25 +0530

    Add changes

--- main.js
+++ main.js
@@ -3,34 +3,46 @@ $("document").ready(function () {
     counter = 0;
     var inWrap = $(".inner-wrapper");
     function inter() {
-        interval_ = setInterval(function () {
-            counter++;
-            if (counter > 2) {
-                counter = 0;
-            }
-            $(".page-item").removeClass("active");
-            $(".page-item").eq(counter).addClass("active");
+        if (interval_ == null) {
 
-            inWrap.animate({ left: "0%" }, 1000, function () {
-                inWrap.css("left", "-100%");
 
-                $(".slide").first().before($(".slide").last());
-            });
-        }, 3000);
+            interval_ = setInterval(function () {
+                console.log(interval_);
+                counter++;
+                if (counter > 2) {
+                    counter = 0;
+                }
+                $(".page-item").removeClass("active");
+                $(".page-item").eq(counter).addClass("active");
+
+                inWrap.animate({ left: "0%" }, 1000, function () {
+                    inWrap.css("left", "-100%");
+
+                    $(".slide").first().before($(".slide").last());
+                });
+            }, 3000);
+        }
     } inter();
 
     $(".prev").on("click", function () {
         clearInterval(interval_);
+        interval_ = null;
+
         counter++;
+        // s
         inWrap.animate({ left: "0%" }, 200, function () {
             inWrap.css("left", "-100%");
+
             inter();
+
+
             $(".slide").first().before($(".slide").last());
         });
     });
 
     $(".next").on("click", function () {
         clearInterval(interval_);
+        interval_ = null;
         counter--;
         inWrap.animate({ left: "-200%" }, 200, function () {
             inWrap.css("left", "-100%");
